0db39005ef816211d93658ce38c7339cd31f61bd
Roman-Dario-Esquivel <Roman-Dario-Esquivel>
2023-02-15 20:04:34 -0300

    ejecitacion py completo 1 corregido

--- ejercicio_1/main.py
+++ ejercicio_1/main.py
@@ -1,25 +1,11 @@
-f = open('archivo.txt','w')
+file = open('mi_primer_archivo.txt', 'w')
+file.write('¡He creado mi primer archivo!\n')
+file.close()
 
-f.write('linea 1\n')
-f.write('linea 2\n')
-f.write('linea 3\n')
-f.write('linea 4\n')
-f.write('linea 5\n')
-f.write('linea 6\n')
-f.close
-f = open('archivo.txt','r')
+file = open('mi_primer_archivo.txt', 'r+')
+file.readline()
+file.write('Esta es la segunda vez que escribo.\n')
 
-print(f.read())
-f.close
-f = open('archivo.txt','a')
-f.write('linea 7\n')
-f.write('linea 8\n')
-f.write('linea 9\n')
-f.write('linea 10\n')
-f.write('linea 11\n')
-f.write('linea 12\n')
-f.close
-f = open('archivo.txt','r')
-
-print(f.read())
-f.close
+file.seek(0)
+print(file.read())
+file.close()
